df1534b162a2d80e8b2a15e2b88d66565d9b6bc3
Sander <Sander@DESKTOP-S3CB981>
2020-12-19 21:55:19 +0100

    Update Generator.js

--- js/Generator.js
+++ js/Generator.js
@@ -10,6 +10,7 @@ window.onload = function () {
     var challengeText = document.getElementById("Challenge-Text")
     
     var kills = ["3", "5", "10", "15", "20", "25"]
+    var specialKillAmounts = ["2", "4", "5", "8", "10", "15"]
     var maps = ["pier", "snowfall", "snowville", "wutville", "2fort", "double cross", "landfall", "sawmill", "turbine", "well", "5gorge", "badlands", "coldfront", "fastlane", "foundry", "freight", "granary", "gullywash", "metalworks", "powerhouse", "process", "snakewater", "sunshine", "vanguard", "well", "yukon", "dustbowl", "egypt", "gorge", "gravel pit", "junction", "mercenary park", "mossrock", "mountain lab", "snowplow", "steel", "standin", "hydro", "badwater basin", "barnblitz", "borneo", "enclosure", "frontier", "gold rush", "hoodoo", "precipice", "snowycoast", "swiftwater", "thunder mountain", "upward", "banana bay", "hightower", "nightfall", "pipeline", "badlands", "koth badlands", "brazil", "harvest", "highpass", "kong king", "lakeside", "lazarus", "nucleus", "probed", "sawmill", "suijin", "viaduct", "doomsday", "dustbowl"]
     var character = ["scout", "soldier", "pyro", "demoman", "heavy", "engie", "medic", "sniper", "spy"]
     var points = ["10", "20", "30", "50", "70", "100"]
@@ -27,10 +28,10 @@ window.onload = function () {
             challengeText.innerHTML = "Play on " + random(maps) + " as " + random(character) + " and get " + random(points) + " points in one game."
             break;
         case 3:
-            challengeText.innerHTML = "Play on " + random(maps) + " and get " + random(kills) + " "+  random(specialKills) + " kills in one round"
+            challengeText.innerHTML = "Play on " + random(maps) + " and get " + random(specialKillAmounts) + " "+  random(specialKills) + " kills in one round"
             break;
         case 4:
-            challengeText.innerHTML = "Play any map and get " + random(kills) + " " + random(specialKills) + " kills in one round"
+            challengeText.innerHTML = "Play any map and get " + random(specialKillAmounts) + " " + random(specialKills) + " kills in one round"
             break;
         default: 
         challengeText.innerHTML = "Play on " + random(maps) + " as " + random(character) + " and get " + random(kills) + " kills in one life."
